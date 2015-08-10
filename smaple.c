e2993eff58e86a37a2e79cab92f5d861704f29aa
roshannist <roshannist@gmail.com>
2015-08-10 06:06:44 -0400

    ch

--- smaple.c
+++ smaple.c
@@ -2,9 +2,17 @@
 #include<stdio.h>
 int main()
 {
-printf("hello world");
-return 0;
-printf ("my name is DON");
-printf ("my name is real");
-
+printf("helflo world");
+return 0;fdf
+printf ("my name is dDONd");
+printf ("my name isf refal");
+printf ("my name ids fdDON");
+printf ("my name isd real");
+printf ("my name dfis DONdf");
+printf ("my namef is real");
+printf ("my namde is DON");
+printf ("my dndfame is real");
+printf ("mydff name is DON");
+printf ("my nadfme is real");
+printf ("my nadfme is real");
 }
